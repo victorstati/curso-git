047cdcd115a08133c0d082d60b13d95cda2d8def
Victor <victorstti@outlook.com>
2018-12-14 09:46:39 -0200

    Acrescentada a mensagem desejando um bom dia

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia");
+            Console.WriteLine("Hello World!");          
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia");
 
             Console.ReadLine();
         }
