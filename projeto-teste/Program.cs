47a2f4bdb4b32b24dfe3accd2d9cac7ed6bc2ecd
Victor <victorstti@outlook.com>
2018-12-14 09:06:36 -0200

    Acrescentada uma mensagem de bom dia

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -8,6 +8,7 @@ namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
 
             Console.ReadLine();
         }
