6836875d8aeb4aea15f76d349f477afd53a421d6
Victor <victorstti@outlook.com>
2018-12-14 09:22:16 -0200

    Acrescentada a mensagem de como estah

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -9,6 +9,7 @@ namespace projeto_teste {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Como você está hoje?");
 
             Console.ReadLine();
         }
